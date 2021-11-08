df692c66e01c8b1367f9ede15ba9974e0e1c6609
yoshimatsu567 <febbasyoungmason@gmail.com>
2021-11-08 09:49:53 +0900

    update

---
+++ dist/entities/repositories/modules/phaseMastRepository.d 2.ts
@@ -0,0 +1,8 @@
+import { PhaseMast, Scalars } from '../..';
+export interface IPhaseMastRepository {
+    addPhase(input: PhaseMast): Promise<PhaseMast>;
+    updatePhase(input: PhaseMast): Promise<PhaseMast>;
+    fetchPhasesByClientID(clientID: Scalars['ID']): Promise<PhaseMast[]>;
+    fetchPhasesByEditedUserID(editedUserID: Scalars['ID']): Promise<PhaseMast[]>;
+    fetchAllPhase(): Promise<PhaseMast[]>;
+}
---
+++ dist/entities/repositories/modules/userMastRepository.d 2.ts
@@ -0,0 +1,8 @@
+import { Scalars, UserMast } from '../../type';
+export interface IUserMastRepository {
+    addUserMast(input: UserMast): Promise<UserMast>;
+    updateUserMast(input: UserMast): Promise<UserMast>;
+    fetchAllUser(): Promise<UserMast[]>;
+    fetchMyUserMast(): Promise<UserMast | null>;
+    fetchUserMastByUserID(userID: Scalars['ID']): Promise<UserMast | null>;
+}
