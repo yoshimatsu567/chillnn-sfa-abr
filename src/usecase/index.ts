4a750d95738373eff6d8b1b314bf4a5dde052d86
yoshimatsu567 <febbasyoungmason@gmail.com>
2021-11-11 09:15:32 +0900

    update

--- src/usecase/index.ts
+++ src/usecase/index.ts
@@ -1,5 +1,5 @@
 export * from './user/userUseCase';
 export * from './client/clientUseCase';
-// export * from './event/eventUseCase';
-// export * from './phase/phaseUseCase';
+export * from './event/eventUseCase';
+export * from './phase/phaseUseCase';
 export * from './search/searchUseCase';
